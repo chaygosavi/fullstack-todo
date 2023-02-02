5d8c9505dfcc52e30ad13a427a1b996839d272f2
Chaitanya Gosavi <chaitanyagosavi540@gmail.com>
2023-02-02 22:32:39 +0530

    will complete later

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [error, setError] = useState(null);
+  const isLogin = false;
+  return (
+    <div className="auth-container">
+      <div className="auth-container-box">
+        <form action="">
+          <h2>{isLogin ? "Please log in" : "Please sign up"}</h2>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          {!isLogin ? (
+            <input type="password" placeholder="confirm password" />
+          ) : (
+            <></>
+          )}
+          <input type="submit" value="" className="create" />
+          {error ? <p>{error}</p> : <></>}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
